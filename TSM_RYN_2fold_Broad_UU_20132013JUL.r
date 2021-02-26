4f35f0c62e59f1a2bcca6bf45b121214c2a5d763
Rachel Yuan Nong Mikkelsen <rachel.yn.mikkelsen@gmail.com>
2021-02-26 11:18:56 +0100

    Create TSM_RYN_2fold_Broad_UU_20132013JUL.r

---
+++ TSM_RYN_2fold_Broad_UU_20132013JUL.r
@@ -0,0 +1,7 @@
+#TSM_RYN_2fold_Broad_UU_2013JUL.r
+#Rachel Yuan Nong Mikkelsen 2021-FEB-26 Happy the new Ox Year !
+#Credits to the Mikkelsen lab at the Broad Insitute, 2012
+#This project was paused due to we found protection strand wasn't exactly as design after initial data presented. 
+#Attached initial data, and follow-up analysis, TBC.
+
+
